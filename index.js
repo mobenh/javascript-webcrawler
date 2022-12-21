eb575625e79babf1e331da071123b609a4d46acb
Mobenul Haq <mhh982@ftr.com>
2022-12-21 09:27:43 -0800

    updated code

--- index.js
+++ index.js
@@ -6,17 +6,21 @@ const puppeteer = require('puppeteer');
 
 const app = express()
 
-//const url = 'https://www.walgreens.com'
-const url = 'https://learnwebcode.github.io/practice-requests/'
+const url = 'https://www.walgreens.com'
+//const url = 'https://learnwebcode.github.io/practice-requests/'
 
 //navigating from a “starting url” to a “listing page” on the website.
 async function navigateToListing(url) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    url += '/store/c/productlist/N=20000910/1/ShopAll=20000910'
     await page.goto(url);
-    await page.click('#inner-focus')
-    const clickedMenu = await page.$eval('#dropdown dropdown__menu', el => el.textContent)
-    console.log(clickedMenu)
+    //await page.click('.menu-trigger');
+    //await page.click('#menu-shop-products');
+    //await page.click('#Household Essentials');
+    //const clickedMenu = await page.$eval('#menu-shop-products', el => el.textContent);
+
+    console.log(url);
     await browser.close();
 }
 
