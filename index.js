b0cf49116a2d3f6a2a768a8267a5aa5b31cf6c33
Moben Haq <moben.h@outlook.com>
2022-12-20 21:51:01 -0800

    fixing index.js

--- index.js
+++ index.js
@@ -6,31 +6,40 @@ const puppeteer = require('puppeteer');
 
 const app = express()
 
-const url = 'https://www.walgreens.com'
+//const url = 'https://www.walgreens.com'
+const url = 'https://learnwebcode.github.io/practice-requests/'
 
 //navigating from a “starting url” to a “listing page” on the website.
-async function scrapeProduct(url) {
+async function navigateToListing(url) {
     const browser = await puppeteer.launch();
-    const page = await browser.newpage();
-    page.goto(url);
-
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.click('#inner-focus')
+    const clickedMenu = await page.$eval('#dropdown dropdown__menu', el => el.textContent)
+    console.log(clickedMenu)
+    await browser.close();
 }
 
-axios(url)
-    .then(response => {
-        const html = response.data
-        const $ = cheerio.load(html)
-        const articles = []
-        $('.fc-item__title', html).each(function() {
-            const title = $(this).text()
-            const url = $(this).find('a').attr('href')
-            articles.push({
-                title,
-                url
-            })
-        })
-        //const artText = articles.jsonValue();
-        console.log({articles})
-    }).catch(err => console.log(err))
+navigateToListing(url);
+
+
+
+// axios(url)
+//     .then(response => {
+//         const html = response.data
+//         const $ = cheerio.load(html)
+//         const articles = []
+//         $('.fc-item__title', html).each(function() {
+//             const title = $(this).text()
+//             const url = $(this).find('a').attr('href')
+//             articles.push({
+//                 title,
+//                 url
+//             })
+//         })
+//         console.log({articles})
+//     }).catch(err => console.log(err))
+
+
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
