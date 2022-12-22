4fb26e3fed636c3c017deaa381ff2ec7b358ac79
Moben Haq <moben.h@outlook.com>
2022-12-21 16:00:11 -0800

    12/21/22 update

--- index.js
+++ index.js
@@ -7,43 +7,41 @@ const puppeteer = require('puppeteer');
 const app = express()
 
 const url = 'https://www.walgreens.com'
-//const url = 'https://learnwebcode.github.io/practice-requests/'
+let url2 = ''
+const attributes = []
 
 //navigating from a “starting url” to a “listing page” on the website.
 async function navigateToListing(url) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    url += '/store/c/productlist/N=20000910/1/ShopAll=20000910'
-    await page.goto(url);
-    //await page.click('.menu-trigger');
-    //await page.click('#menu-shop-products');
-    //await page.click('#Household Essentials');
-    //const clickedMenu = await page.$eval('#menu-shop-products', el => el.textContent);
-
-    console.log(url);
+    url2 = url + '/store/c/productlist/N=20000910/1/ShopAll=20000910'
+    await page.goto(url2);
+    scrapePage(url2);
     await browser.close();
 }
 
+//collecting a small number of specified attributes on the page.
+function scrapePage(url){
+        axios(url)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+        
+            $('.product-container', html).each(function () {
+                const productNames = $(this).text()
+                const prices = $(this).find('.product__price-contain').text()
+                const imgURL = $(this).find('a').attr('href')
+                const url = $(this).find('img').attr('src')
+                attributes.push({
+                    productNames,
+                    prices,
+                    imgURL,
+                    url
+                })
+            })
+            console.log(attributes)
+        }).catch(err => console.log(err))
+}
+
 navigateToListing(url);
-
-
-
-// axios(url)
-//     .then(response => {
-//         const html = response.data
-//         const $ = cheerio.load(html)
-//         const articles = []
-//         $('.fc-item__title', html).each(function() {
-//             const title = $(this).text()
-//             const url = $(this).find('a').attr('href')
-//             articles.push({
-//                 title,
-//                 url
-//             })
-//         })
-//         console.log({articles})
-//     }).catch(err => console.log(err))
-
-
-
-app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
+console.log('Please wait, scraping in progress')
